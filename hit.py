83415b7efef276c6fb5c4c8c585b8496d3212fa9
Chinna reddy <chinnareddy2106@gmail.com>
2022-12-01 16:07:30 +0530

    added requirments

--- hit.py
+++ hit.py
@@ -1,47 +1,47 @@
- git remote -v
- git remote remove origin
- git remore -v
- git log
- git remote add origin https://github.com/Nani9966/ML_12.git
- git remote -v
-git log
-git push origin main
-git pull orgin main
-ls -a >>>>.git
-cd .git 
-ls()  >>>>FETCH_HEAD,HEAD,hook index info logs objects packed_refs refs 
-cd  logs 
-ls 
-cd refs
-cat HEAD
-cd .. 
-cd refs >> head and remotes
-cd remotes
-ls  >> origin
-cd origin
-ls >> __main__
-cat main
+#  git remote -v
+#  git remote remove origin
+#  git remore -v
+#  git log
+#  git remote add origin https://github.com/Nani9966/ML_12.git
+#  git remote -v
+# git log
+# git push origin main
+# git pull orgin main
+# ls -a >>>>.git
+# cd .git 
+# ls()  >>>>FETCH_HEAD,HEAD,hook index info logs objects packed_refs refs 
+# cd  logs 
+# ls 
+# cd refs
+# cat HEAD
+# cd .. 
+# cd refs >> head and remotes
+# cd remotes
+# ls  >> origin
+# cd origin
+# ls >> __main__
+# cat main
 
 
 
-git log
-git reset --soft 6afd
-git log
+# git log
+# git reset --soft 6afd
+# git log
 
-git add .
-git status
-git commit -m "this is our first over version"
-git config --global user.email "chinnareddy2106@gmail.com"
-git config --global user.name "Chinna reddy"
-git commit -m "this is our first over version"
-git push origin main -f
+# git add .
+# git status
+# git commit -m "this is our first over version"
+# git config --global user.email "chinnareddy2106@gmail.com"
+# git config --global user.name "Chinna reddy"
+# git commit -m "this is our first over version"
+# git push origin main -f
 
 
-git add .
-git status
-git commit -m "taddeed the gitigone filen"
-git status
+# git add .
+# git status
+# git commit -m "taddeed the gitigone filen"
+# git status
 
-git add README.md
-git status
-git commit -m
+# git add README.md
+# git status
+# git commit -m
