27b49146136b8342307420b72340ad47aa21b900
Chinna reddy <chinnareddy2106@gmail.com>
2022-12-01 14:54:25 +0530

    this is our first over version

---
+++ hit.py
@@ -0,0 +1,31 @@
+ git remote -v
+ git remote remove origin
+ git remore -v
+ git log
+ git remote add origin https://github.com/Nani9966/ML_12.git
+ git remote -v
+git log
+git push origin main
+git pull orgin main
+ls -a >>>>.git
+cd .git 
+ls()  >>>>FETCH_HEAD,HEAD,hook index info logs objects packed_refs refs 
+cd  logs 
+ls 
+cd refs
+cat HEAD
+cd .. 
+cd refs >> head and remotes
+cd remotes
+ls  >> origin
+cd origin
+ls >> __main__
+cat main
+
+
+
+git log
+git reset --soft 6afd
+git log
+
+git add .
